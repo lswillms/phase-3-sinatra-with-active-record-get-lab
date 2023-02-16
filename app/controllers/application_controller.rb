32d9d6058700454c1c37e6efc9f875601740d948
Latoshia Williams <lswillms@yahoo.com>
2023-02-16 17:40:44 -0600

    commit tree eddb64675931049ebb5fc849626c7ac8b1f58643

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+set :default_content_type, "application/json"
+
+get '/bakeries' do 
+   bake = Bakery.all
+   bake.to_json
+end
+
+get '/bakeries/:id' do
+  bake = Bakery.find(params[:id])
+  bake.to_json(include: :baked_goods)
+end
+
+get '/baked_goods/by_price' do 
+  bake = BakedGood.order(price: :desc)
+  bake.to_json
+end
+
+get '/baked_goods/most_expensive' do 
+  bake = BakedGood.order(price: :desc).first
+
+  bake.to_json
+end
+
+
 
 end
